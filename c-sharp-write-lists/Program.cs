556396c78f8f7065e6706ce8c810eaf884a74c1b
hacuthbert <79534190+hacuthbert@users.noreply.github.com>
2025-04-09 19:19:56 -0400

    version 1 program utilities and csproj

---
+++ c-sharp-write-lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System.Globalization;
+using c_sharp_write_lists;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberOfRecords = 100;
+        int startingNumber = 62001;
+        int startingSequence = 62001;
+        string kitTypeCode = "A";
+        int lastNumber = startingNumber + numberOfRecords;
+
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string kitListName = $"KitList - kit type {kitTypeCode} {startingNumber}to{lastNumber}.csv";
+        string fullPath = Path.Combine(desktopPath, kitListName);
+
+        List<Utilities.KitList> records = new List<Utilities.KitList>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            records.Add(new Utilities.KitList( startingNumber + i, startingSequence + i, kitTypeCode ));
+        }
+
+
+        using (var writer = new StreamWriter(fullPath))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(records);
+        }
+
+        Console.WriteLine(fullPath);
+    }
+}
---
+++ c-sharp-write-lists/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_write_lists
+{
+    internal class Utilities
+    {
+        public class KitList
+        {
+            public int KitNumber { get; set; }
+            public int SequenceNumber { get; set; }
+            public string KitType { get; set; }
+
+            public KitList(int kitnum, int seqnum, string kittype)
+            {
+                KitNumber = kitnum;
+                SequenceNumber = seqnum;
+                KitType = kittype;
+            }
+        }
+    }
+}
